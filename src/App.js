90f486a6f8d07bbf07ad154e0f8dcb87a2082b50
Tamal Das <gyansujan69@gmail.com>
2021-08-27 13:19:03 +0530

    Added navbar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <section className="navbar-sec">
+        <div class="container-fluid">
+          <nav class="navbar  navbar-expand-lg navbar-light">
+            {/* <!-- navbar brand --> */}
+            <a href="/" class="navbar-brand" id="nb">
+              {" "}
+              Text-Utils
+            </a>
+
+            {/* <!-- hamburger menu --> */}
+            <button
+              class="navbar-toggler me-1"
+              id="nbtn"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navtoggle"
+              aria-controls="navtoggle"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span class="navbar-toggler-icon"></span>
+            </button>
+
+            {/* <!-- navbar er baaki shob jinish --> */}
+            <div class="collapse navbar-collapse" id="navtoggle">
+              <ul class="navbar-nav ms-auto">
+                <li class="nav-item">
+                  <a class="nav-link active" href="/">
+                    {" "}
+                    Home
+                  </a>
+                </li>
+
+                <li class="nav-item" id="lg">
+                  <a class="nav-link" href="/">
+                    {" "}
+                    Education{" "}
+                  </a>
+                </li>
+
+                <li class="nav-item" id="lg">
+                  <a class="nav-link" href="/">
+                    {" "}
+                    Projects{" "}
+                  </a>
+                </li>
+
+                <li class="nav-item" id="lg">
+                  <a class="nav-link" href="/">
+                    {" "}
+                    Contact Me{" "}
+                  </a>
+                </li>
+              </ul>
+            </div>
+            {/* <!-- end of navbar --> */}
+          </nav>
+        </div>
+      </section>
+    </>
   );
 }
 
